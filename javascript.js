8c35ca116b2602d1cccfdb44a1ffe3caf85fbb2b
Cyrus Bigham <bighamcyrus@gmail.com>
2022-06-18 09:53:31 -0700

    button works

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-javascript.javascript
+
+var startBtn = document.querySelector("#start");
+
+function startquestions(){
+    var question1 = prompt("How do you set up a new file to be an HTML file?")
+
+    if (question1 !== "<!DOCTYPE html>"){
+        alert("incorret, try again")
+        return
+    }
+}
+
+function startquiz() {
+    var questions = startquestions()
+    
+}
+
+startBtn.addEventListener("click", startquiz);
